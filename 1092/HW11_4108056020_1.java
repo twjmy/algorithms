383828e692f93d1311ca4d52743c5ed07a4dfb5e
VJ <twjmy@msn.com>
2021-06-12 17:31:53 +0800

    update HW11_1,2

--- 1092/HW11_4108056020_1.java
+++ 1092/HW11_4108056020_1.java
@@ -1,12 +1,8 @@
 public class HW11_4108056020_1 extends GroupCounting{
 	final class Slot{
-		final Slot NEXT;
-		final String K;
-		final int V;
-		Slot(final String k, final int v, final Slot next){
-			this.K = k;
-			this.V = v;
-			this.NEXT = next;
+		final Slot NEXT; final String K; final int V;
+		Slot(final Slot next, final String k, final int v){
+			this.NEXT = next; this.K = k; this.V = v;
 		}
 	}
 	public int count(final String[] A, final String[] B){
@@ -15,19 +11,18 @@ public class HW11_4108056020_1 extends GroupCounting{
 		final Slot[] BUCKET = new Slot[BUCKEND+1];
 		for(int i = 0; i < SIZE; i++) UNION[i] = i;
 		int nodeN = 0, unionN = 0;
-		for(int i = 0, a, b; i<LEN; i++){
-			a = b = -1;
-			for(Slot pivot = BUCKET[A[i].hashCode() & BUCKEND]; pivot!=null; pivot=pivot.NEXT)
+		for(int i = 0, a = -1, b = -1; i<LEN; i++, a = b = -1){
+			for(Slot pivot = BUCKET[BUCKEND & A[i].hashCode()]; pivot!=null; pivot=pivot.NEXT)
 				if(pivot.K.equals(A[i])) { a = pivot.V; break; }
-			for(Slot pivot = BUCKET[B[i].hashCode() & BUCKEND]; pivot!=null; pivot=pivot.NEXT)
+			for(Slot pivot = BUCKET[BUCKEND & B[i].hashCode()]; pivot!=null; pivot=pivot.NEXT)
 				if(pivot.K.equals(B[i])) { b = pivot.V; break; }
 			if(a == -1){
-				final int BN = A[i].hashCode() & BUCKEND;
-				BUCKET[BN] = new Slot(A[i], (a = nodeN++), BUCKET[BN]);
+				final int BN = BUCKEND & A[i].hashCode();
+				BUCKET[BN] = new Slot(BUCKET[BN], A[i], (a = nodeN++));
 			}
 			if(b == -1){
-				final int BN = B[i].hashCode() & BUCKEND;
-				BUCKET[BN] = new Slot(B[i], (b = nodeN++), BUCKET[BN]);
+				final int BN = BUCKEND & B[i].hashCode();
+				BUCKET[BN] = new Slot(BUCKET[BN], B[i], (b = nodeN++));
 			}
 			while(UNION[a] != a) a = UNION[a] = UNION[UNION[a]];
 			while(UNION[b] != b) b = UNION[b] = UNION[UNION[b]];
--- 1092/HW11_4108056020_2.java
+++ 1092/HW11_4108056020_2.java
@@ -1,31 +1,23 @@
 public class HW11_4108056020_2 extends GroupCounting{
 	public int count(final String[] A, final String[] B){
-		final int LEN = A.length, UNION[] = new int[10000000];
+		final int LEN = A.length, UNION[] = new int[2000000];
 		int nodeN = 0, unionN = 0;
 		for(int i = 0, a, b, t; i < LEN; i++){
-			a = Integer.parseInt(A[i])+1; b = Integer.parseInt(B[i])+1;
-			if(UNION[a] == 0){ UNION[a] = -1; nodeN++; }
-			else{
+			if(UNION[a = Integer.parseInt(A[i])+1] != 0){
 				for(t = a; UNION[t] > -1; t = UNION[t]);
-				for(int j, k = a; k != t; k = j){
-					j = UNION[k]; UNION[k] = t;
-				}
+				for(int j, k = a; k != t; j = UNION[k], UNION[k] = t, k = j);
 				a = t;
-			}
-			if(UNION[b] == 0){ UNION[b] = -1; nodeN++; }
-			else{
+			}else{ UNION[a] = -1; nodeN++; }
+			if(UNION[b = Integer.parseInt(B[i])+1] != 0){
 				for(t = b; UNION[t] > -1; t = UNION[t]);
-				for(int j, k = b; k != t; k = j){
-					j = UNION[k]; UNION[k] = t;
-				}
+				for(int j, k = b; k != t; j = UNION[k], UNION[k] = t, k = j);
 				b = t;
-			}
+			}else{ UNION[b] = -1; nodeN++; }
 			if(a == b) continue;
-			t = UNION[a] + UNION[b];
 			if(UNION[a] > UNION[b]){
-				UNION[a] = b; UNION[b] = t;
+				UNION[b] += UNION[a]; UNION[a] = b;
 			}else{
-				UNION[b] = a; UNION[a] = t;
+				UNION[a] += UNION[b]; UNION[b] = a;
 			}
 			unionN++;
 		}
